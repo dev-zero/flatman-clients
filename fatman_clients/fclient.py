e59138b3d0350d64a25f7a0e330e1f83bfdf97f5
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2017-01-05 10:50:17 +0100

    client: implement artifact listing and downloading

--- fatman_clients/fclient.py
+++ fatman_clients/fclient.py
@@ -4,6 +4,8 @@ import json
 import re
 from io import BytesIO
 from uuid import UUID
+import os
+from os import path
 
 import requests
 from requests.packages import urllib3
@@ -396,12 +398,63 @@ def task(ctx):
     ctx.obj['task_url'] = '{url}/api/v2/tasks'.format(**ctx.obj)
 
 
-@task.command('upload')
+@task.command('list-files')
+@click.argument('task_id', type=UUID)
+@click.pass_context
+def task_list_files(ctx, task_id):
+    """List all files associated with the specified task"""
+
+    req = ctx.obj['session'].get(ctx.obj['task_url'] + '/{}'.format(task_id))
+    req.raise_for_status()
+    task_content = req.json()
+
+    for infile in task_content['infiles']:
+        click.echo("-> {}".format(infile['name']))
+
+    for outfile in task_content['outfiles']:
+        click.echo("<- {}".format(outfile['name']))
+
+
+@task.command('download-files')
+@click.argument('task_id', type=UUID)
+@click.pass_context
+def task_download_files(ctx, task_id):
+    """
+    Creates a new directory task_<task_id> in the
+    current working directory and downloads all files
+    """
+
+    target_dir = "task_{}".format(task_id)
+
+    req = ctx.obj['session'].get(ctx.obj['task_url'] + '/{}'.format(task_id))
+    req.raise_for_status()
+    task_content = req.json()
+
+    os.mkdir(target_dir)
+    os.mkdir(path.join(target_dir, 'infiles'))
+    os.mkdir(path.join(target_dir, 'outfiles'))
+
+    for direction in ['infiles', 'outfiles']:
+        for artifact in task_content[direction]:
+            target_fn = path.join(target_dir, direction, artifact['name'])
+            click.echo("downloading {} to {}..".format(artifact['name'], target_fn), nl=False)
+
+            req = ctx.obj['session'].get(artifact['_links']['download'], stream=True)
+            req.raise_for_status()
+
+            with open(target_fn, 'wb') as fhandle:
+                for chunk in req.iter_content(1024):
+                    fhandle.write(chunk)
+
+            click.echo(" done")
+
+
+@task.command('upload-artifact')
 @click.argument('task_id', type=UUID)
 @click.argument('filename', type=click.File(mode='rb'))
 @click.argument('name', type=str)
 @click.pass_context
-def task_uplodate(ctx, task_id, filename, name):
+def task_upload_artifact(ctx, task_id, filename, name):
     """Upload artifacts for given task using the specified name"""
 
     req = ctx.obj['session'].get(ctx.obj['task_url'] + '/{}'.format(task_id))
