2fab159f4379c8735c3b2cd6f48b3d6776f6f579
Tiziano Müller <tm@dev-zero.ch>
2017-04-19 08:55:45 +0200

    client: implement CR.D for testresult collections

--- fatman_clients/fclient.py
+++ fatman_clients/fclient.py
@@ -9,6 +9,7 @@ from os import path
 import sys
 import csv
 from collections import OrderedDict
+import textwrap
 
 import requests
 from requests.packages import urllib3
@@ -718,6 +719,133 @@ def testresult_generate_results(ctx, update, ids):
         req.raise_for_status()
 
     # TODO: implement result parsing and waiting for finish
+
+
+@cli.group()
+@click.pass_context
+def trcollections(ctx):
+    """Manage test result collectionss"""
+    ctx.obj['trcollections_url'] = '{url}/api/v2/testresultcollections'.format(**ctx.obj)
+
+
+@trcollections.command('list')
+@click.pass_context
+def testresult_list(ctx):
+    """
+    List test result collections
+    """
+
+    req = ctx.obj['session'].get(ctx.obj['trcollections_url'])
+    req.raise_for_status()
+    trcolls = req.json()
+
+    table_data = [
+        ['id', 'name', 'number of results', 'description'],
+        ]
+
+    for trcoll in trcolls:
+        table_data.append([
+            trcoll['id'],
+            "\n".join(textwrap.wrap(trcoll['name'], width=20)),
+            trcoll['testresult_count'],
+            "\n".join(textwrap.wrap(trcoll['desc'], width=40)),
+            ])
+
+    if sys.stdout.isatty():
+        table_instance = SingleTable(table_data)
+    else:
+        table_instance = AsciiTable(table_data)
+    click.echo(table_instance.table)
+
+
+@trcollections.command('show')
+@click.argument('id', type=UUID, required=True)
+@click.pass_context
+def testresult_show(ctx, id):
+    """
+    Show details for the specified collection
+    """
+
+    req = ctx.obj['session'].get(ctx.obj['trcollections_url'] + '/%s' % id)
+    req.raise_for_status()
+    trcoll = req.json()
+
+    click.echo("Name: {name}".format(**trcoll))
+    click.echo("Description:\n{desc}\n".format(**trcoll))
+
+    click.echo("Testresults ({testresult_count}):\n".format(**trcoll))
+
+    table_data = [
+        ['id', 'test', 'data'],
+        ]
+
+    for tr in trcoll['testresults']:
+        entry = [tr['id'], tr['test']]
+
+        if 'element' in tr['data']:
+            entry.append("element: {element}".format(**tr['data']))
+
+        table_data.append(entry)
+
+    if sys.stdout.isatty():
+        table_instance = SingleTable(table_data)
+    else:
+        table_instance = AsciiTable(table_data)
+    click.echo(table_instance.table)
+
+
+@trcollections.command('create')
+@click.option('--name', type=str, required=True, prompt=True)
+@click.option('--desc', type=str, required=True, prompt=True)
+@click.option('--copy-from', type=UUID, required=False,
+              help="copy test results from another collection")
+@click.option('--copy-from-exclude', type=UUID, required=False, multiple=True,
+              help="exclude the specified test result(s) from the collection to copy from")
+@click.option('--include', type=UUID, required=False, multiple=True,
+              help="include the specified test result(s) in the new collection")
+@click.pass_context
+def testresult_create(ctx, name, desc, copy_from, copy_from_exclude, include):
+    """
+    Create a test result collection
+    """
+
+    # populate the results to be added by the include list,
+    # converting the UUID objects back to strings while at it
+    results = [str(i) for i in include] if include else []
+
+    if copy_from:
+        req = ctx.obj['session'].get(ctx.obj['trcollections_url'] + '/%s' % copy_from)
+        req.raise_for_status()
+        trcoll = req.json()
+
+        excludes = [str(i) for i in copy_from_exclude] if copy_from_exclude else []
+        results += [tr['id'] for tr in trcoll['testresults'] if tr['id'] not in excludes]
+
+    payload = {
+        'name': name,
+        'desc': desc,
+        'testresults': results,
+        }
+
+    req = ctx.obj['session'].post(ctx.obj['trcollections_url'], json=payload)
+    req.raise_for_status()
+    trcoll = req.json()
+
+    click.echo("done, the assigned ID for the new collection: {id}".format(**trcoll))
+
+
+@trcollections.command('delete')
+@click.argument('id', type=UUID, required=True)
+@click.pass_context
+def testresult_create(ctx, id):
+    """
+    Delete a test result collection (does not remove test results)
+    """
+
+    req = ctx.obj['session'].delete(ctx.obj['trcollections_url'] + '/%s' % id)
+    req.raise_for_status()
+
+    click.echo("done")
 
 
 @cli.group(invoke_without_command=True)
